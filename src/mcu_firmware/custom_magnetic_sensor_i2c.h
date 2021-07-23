210c7671a313dafd741f000f67cfa4a9f8bdb821
Tomasz Cybulski <tomasz.cybulski@hotmail.com>
2021-07-23 23:31:17 +0200

    Custom magnetic sensor with hardware selection of connected sensor. Example code.

---
+++ src/mcu_firmware/custom_magnetic_sensor_i2c.h
@@ -0,0 +1,29 @@
+#include "sensors/MagneticSensorI2C.h"
+
+class CustomMagneticSensorI2C : public MagneticSensorI2C {
+ public:
+  CustomMagneticSensorI2C(uint8_t chip_address, int bit_resolution,
+                          uint8_t angle_register_msb, int msb_bits_used,
+                          int this_sda_pin, int other_sda_pin)
+      : MagneticSensorI2C(chip_address, bit_resolution, angle_register_msb,
+                          msb_bits_used),
+        this_sda_pin_(this_sda_pin),
+        other_sda_pin_(other_sda_pin) {}
+
+  CustomMagneticSensorI2C(MagneticSensorI2CConfig_s config, int this_sda_pin,
+                          int other_sda_pin)
+      : MagneticSensorI2C(config),
+        this_sda_pin_(this_sda_pin),
+        other_sda_pin_(other_sda_pin) {}
+
+  void activate();
+
+  /** get current angle (rad) */
+  float getAngle() override;
+  /** get current angular velocity (rad/s)*/
+  float getVelocity() override;
+
+ private:
+  int this_sda_pin_;
+  int other_sda_pin_;
+};
---
+++ src/mcu_firmware/main.cc
@@ -0,0 +1,52 @@
+#include "Arduino.h"
+#include "SimpleFOC.h"
+#include "custom_magnetic_sensor_i2c.h"
+
+// MagneticSensorI2C(uint8_t _chip_address, float _cpr, uint8_t
+// _angle_register_msb)
+//  chip_address  I2C chip address
+//  bit_resolution  resolution of the sensor
+//  angle_register_msb  angle read register msb
+//  bits_used_msb  number of used bits in msb register
+//
+// make sure to read the chip address and the chip angle register msb value from
+// the datasheet also in most cases you will need external pull-ups on SDA and
+// SCL lines!!!!!
+//
+// For AS5058B
+// MagneticSensorI2C sensor = MagneticSensorI2C(0x40, 14, 0xFE, 8);
+
+// Example of AS5600 configuration
+CustomMagneticSensorI2C sensor1 = CustomMagneticSensorI2C(AS5600_I2C, A0, A1);
+CustomMagneticSensorI2C sensor2 = CustomMagneticSensorI2C(AS5600_I2C, A1, A0);
+
+void setup() {
+  // monitoring port
+  Serial.begin(115200);
+
+  // configure i2C
+  Wire.setClock(400000);
+  // initialise magnetic sensor hardware
+  sensor1.activate();
+  sensor1.init();
+
+  Serial.println("Sensor 1 ready");
+  _delay(1000);
+
+  sensor2.activate();
+  sensor2.init();
+
+  Serial.println("Sensor 2 ready");
+  _delay(1000);
+}
+
+void loop() {
+  // display the angle and the angular velocity to the terminal
+  Serial.print(sensor1.getAngle());
+  Serial.print("\t");
+  Serial.println(sensor1.getVelocity());
+  Serial.print("\t");
+  Serial.print(sensor2.getAngle());
+  Serial.print("\t");
+  Serial.println(sensor2.getVelocity());
+}
