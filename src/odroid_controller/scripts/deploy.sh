f2b44df96a00564a46b838e1877d935e24be7ae5
Tomasz Cybulski <tomasz.cybulski@hotmail.com>
2021-07-27 00:42:14 +0200

    Only flash microcontrollers when firmware has changed

--- src/odroid_controller/scripts/deploy.sh
+++ src/odroid_controller/scripts/deploy.sh
@@ -3,5 +3,7 @@
 RUNFILES=${BASH_SOURCE[0]}.runfiles
 DATA_FILES=$RUNFILES/__main__/src
 
-rsync -irvzPLt --exclude=odroid_controller/scripts/deploy.sh ${DATA_FILES}/ robot@192.168.0.12:/home/robot/control
-#ssh robot@192.168.0.12 "cd /home/robot/control/odroid_controller/scripts; ./upload_firmware.sh"
+RSYNC_OUTPUT=$(rsync -irvzPLt --exclude=odroid_controller/scripts/deploy.sh ${DATA_FILES}/ robot@192.168.0.12:/home/robot/control)
+if [[ $RSYNC_OUTPUT == *"mcu_firmware/leg_hex.hex"* ]]; then
+  ssh robot@192.168.0.12 "cd /home/robot/control/odroid_controller/scripts; ./upload_firmware.sh"
+fi
