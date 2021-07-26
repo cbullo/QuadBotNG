f6eba5998e8430f6acccb4a125950f05780b0018
Tomasz Cybulski <tomasz.cybulski@hotmail.com>
2021-07-27 00:36:53 +0200

    Run firmware update after uploading.

--- src/odroid_controller/scripts/deploy.sh
+++ src/odroid_controller/scripts/deploy.sh
@@ -3,4 +3,5 @@
 RUNFILES=${BASH_SOURCE[0]}.runfiles
 DATA_FILES=$RUNFILES/__main__/src
 
-rsync -rvzPL --exclude=odroid_controller/scripts/deploy.sh ${DATA_FILES}/ robot@192.168.0.12:/home/robot/control
+rsync -irvzPLt --exclude=odroid_controller/scripts/deploy.sh ${DATA_FILES}/ robot@192.168.0.12:/home/robot/control
+#ssh robot@192.168.0.12 "cd /home/robot/control/odroid_controller/scripts; ./upload_firmware.sh"
