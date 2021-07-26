234929cc084a28ce8dea534fa11279341bcb2e25
Tomasz Cybulski <tomasz.cybulski@hotmail.com>
2021-07-27 00:43:53 +0200

    Small cleaning up

--- src/odroid_controller/scripts/deploy.sh
+++ src/odroid_controller/scripts/deploy.sh
@@ -3,7 +3,9 @@
 RUNFILES=${BASH_SOURCE[0]}.runfiles
 DATA_FILES=$RUNFILES/__main__/src
 
-RSYNC_OUTPUT=$(rsync -irvzPLt --exclude=odroid_controller/scripts/deploy.sh ${DATA_FILES}/ robot@192.168.0.12:/home/robot/control)
+TARGET_ADDRESS=robot@192.168.0.12
+
+RSYNC_OUTPUT=$(rsync -irvzPLt --exclude=odroid_controller/scripts/deploy.sh ${DATA_FILES}/ ${TARGET_ADDRESS}:/home/robot/control)
 if [[ $RSYNC_OUTPUT == *"mcu_firmware/leg_hex.hex"* ]]; then
-  ssh robot@192.168.0.12 "cd /home/robot/control/odroid_controller/scripts; ./upload_firmware.sh"
+  ssh ${TARGET_ADDRESS} "cd /home/robot/control/odroid_controller/scripts; ./upload_firmware.sh"
 fi
