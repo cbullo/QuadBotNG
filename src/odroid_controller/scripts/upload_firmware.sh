aa808413b70682607e4c721d78b46db4c7e5d591
Tomasz Cybulski <tomasz.cybulski@hotmail.com>
2021-07-27 00:05:42 +0200

    Fix avrdude params. Update yq to v4. Fix firmware dependencies.

--- src/odroid_controller/scripts/upload_firmware.sh
+++ src/odroid_controller/scripts/upload_firmware.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+FILE=../../mcu_firmware/leg_hex.hex
 IFS='
 '
-for x in `yq r robot_config.yaml controllers.*.controller_address`; do 
-avrdude -c arduino -p $$1 -D -V -U flash:w:$$2:i -U flash:v:$$2:i -b 57600 -P $x
+for x in $(yq e '.controllers.*.controller_address' ../config/robot_config.yaml); do
+  avrdude -c arduino -p m328p -D -v -V -U flash:w:${FILE}:i -U flash:v:${FILE}:i -b 57600 -P $x
 done
